32903847e2e823e1e3b9ca459b204f53f350ac28
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-11-26 16:37:20 +0200

    created contacts.js script logic

--- contacts.js
+++ contacts.js
@@ -1,6 +1,62 @@
-function test() {
-    return Date.now();
+const path = require('path');
+const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid');
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+async function readContacts() { 
+    try {
+        const contacts = await fs.readFile(contactsPath, 'utf8');
+        const parsedContacts = JSON.parse(contacts);
+        return parsedContacts;
+    } catch (error) {
+        console.error(error)
+    }
 }
+
+async function listContacts() {
+    contactList = await readContacts();
+    // console.table(contactList);
+    return contactList;
+}
+// listContacts();
+
+async function getContactById(contactId) {
+    const contacts = await readContacts();
+    // console.log(contacts);
+    const foundContact = contacts.find(el => String(el.id) === String(contactId));
+    // console.log(foundContact);
+    return foundContact;
+}
+// getContactById(2);
+
+async function removeContact(contactId) {
+    const contacts = await readContacts();
+    const deletedContact = contacts.find(el => String(el.id) === String(contactId));
+    const newContacts = contacts.filter(el => String(el.id) !== String(contactId));
+    await fs.writeFile(contactsPath, JSON.stringify(newContacts), 'utf8');
+    // console.table(newContacts);
+    console.log(deletedContact);
+    return deletedContact;
+}
+// removeContact(1);
+
+async function addContact(name, email, phone) {
+    const contacts = await readContacts();
+    const newContact = { id: uuidv4(), name, email, phone };
+    // console.log(newContact);
+    // console.log(contacts);
+    // const newContacts = contacts.push(newContact);
+    contacts.push(newContact);
+    // console.table(newContacts);
+    // await fs.appendFile( contactsPath, JSON.stringify(newContact) )
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log(newContact);
+}
+// addContact('testname', 'testemail', 'testphone');
+
 module.exports = {
-    test
+    listContacts,
+    getContactById
+
 }
--- index.js
+++ index.js
@@ -1,5 +1,147 @@
-const { test } = require('./contacts');
-console.log(test());
+const { listContacts, getContactById } = require('./contacts')
+
+
+// listContacts();
+// getContactById(3)
+
+// const { Command } = require("commander");
+// const program = new Command();
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
+
+// program.parse(process.argv);
+
+// const argv = program.opts();
+
+// // TODO: рефакторить
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       // ...
+//       break;
+
+//     case "get":
+//       // ... id
+//       break;
+
+//     case "add":
+//       // ... name email phone
+//       break;
+
+//     case "remove":
+//       // ... id
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
+
+// invokeAction(argv);
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const app = express();
+// const PORT = 8081
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use(express.static('public'))
+
+// app.use((req, res, next) => { 
+//     console.log(`${req.method}`);
+//     next();
+// })
+// app.get('/home', (req, res) => { 
+//     res.sendStatus(200);
+// })
+// app.use((req, res) => { 
+//     res.send('global responsasdaess')
+// })
+// app.listen(PORT, (err) => { 
+//      if (err) {
+//          console.error(`Error at server launch : ${err}`);
+//      }
+//      console.log(`Server is working at: ${PORT}`)
+//  })
+
+
+// const fs = require('fs').promises;
+// const path = require('path');
+// const http = require('http');
+// const PORT = 8081
+
+// const requestHandler = (request, response) => { 
+//     console.log(request);
+//     response.writeHead(200, { 'Content-type': 'text/html' });
+//     response.end('<h1>RESPONSE</h1>')
+
+// }
+// const server = http.createServer(requestHandler)
+// server.listen(PORT, (err) => { 
+//     if (err) {
+//         console.error(`Error at server launch : ${err}`);
+//     }
+//     console.log(`Server is working at: ${PORT}`)
+// });
+
+// const { listContacts } = require('./contacts');
+
+// listContacts();
+
+// (async () => {
+//     try {
+//         const data = await fs.readFile(path.resolve('./data.txt'), 'utf8');
+//         console.log(data);
+
+//         // const newContent = `${data} new`;
+//         // await fs.writeFile(path.resolve('./data.txt'), newContent, 'utf8');
+
+//         // await fs.rename(path.resolve('./data.txt'), path.resolve('./data.txt'))
+//     } catch (error) {
+//         console.error(error);
+        
+//     }
+ 
+// })();
+
+
+// fs.readFile(path.resolve('./data.txt'), 'utf8')
+//     .then(data => { 
+//         console.log(data);
+//     })
+//     .catch(error => console.error(error))
+
+// fs.readFile(path.resolve('./data.txt'), 'utf8', (err, data) => { 
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(data)
+// })
+
+// fs.readFile('./data.txt', 'utf8', (err, data) => { 
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(data)
+// })
+// console.log(path.resolve('contacts.js'));
+
+// const { test } = require('./contacts');
+// console.log(test());
 // globalThis.testData = '123'
 // console.log(global.testData);
 // console.log(process.argv);
