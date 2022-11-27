6e381c7b50ae173ce74bd9be28d9691e43418704
Alexei Solotvinskiy <97736656+alexvile@users.noreply.github.com>
2022-11-27 18:54:03 +0200

    remove console log and extra files

--- contacts.js
+++ contacts.js
@@ -16,45 +16,30 @@ async function readContacts() {
 
 async function listContacts() {
     contactList = await readContacts();
-    // console.table(contactList);
     return contactList;
 }
-// listContacts();
 
 async function getContactById(contactId) {
     const contacts = await readContacts();
-    // console.log(contacts);
     const foundContact = contacts.find(el => String(el.id) === String(contactId));
-    // console.log(foundContact);
     return foundContact;
 }
-// getContactById(2);
 
 async function removeContact(contactId) {
     const contacts = await readContacts();
     const deletedContact = contacts.find(el => String(el.id) === String(contactId));
     const newContacts = contacts.filter(el => String(el.id) !== String(contactId));
     await fs.writeFile(contactsPath, JSON.stringify(newContacts), 'utf8');
-    // console.table(newContacts);
-    // console.log(deletedContact);
     return deletedContact;
 }
-// removeContact(1);
 
 async function addContact(name, email, phone) {
     const contacts = await readContacts();
     const newContact = { id: uuidv4(), name, email, phone };
-    // console.log(newContact);
-    // console.log(contacts);
-    // const newContacts = contacts.push(newContact);
     contacts.push(newContact);
-    // console.table(newContacts);
-    // await fs.appendFile( contactsPath, JSON.stringify(newContact) )
     await fs.writeFile(contactsPath, JSON.stringify(contacts));
-    // console.log(newContact);
     return newContact;
 }
-// addContact('testname', 'testemail', 'testphone');
 
 module.exports = {
     listContacts,
